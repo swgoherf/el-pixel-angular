4868337bb433432955cc9cb69b65330f67197557
Игорь <igor1sorokoletov@gmail.com>
2026-06-23 14:28:57 +0300

    complite tasks 23.06

--- el-pixel-angular/src/app/app.ts
+++ el-pixel-angular/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [Home],
+  template: `<main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/public/logo.svg" alt="logo" aria-hidden="true" />
+    </header>
+    <section>
+      <app-home />
+    </section>
+  </main>`,
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('el-pixel-angular');
+  title = 'Default';
 }
