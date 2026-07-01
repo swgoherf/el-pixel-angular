83832a1c7f90f6995fb1de4d6db3daab22e80eed
Игорь <igor1sorokoletov@gmail.com>
2026-07-01 10:19:01 +0300

    complete task 30.06

--- el-pixel-angular/src/app/home/home.ts
+++ el-pixel-angular/src/app/home/home.ts
@@ -1,31 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HousingLocation } from '../housing-location/housing-location';
 import { HousingLocationInfo } from '../housinglocation';
+import { HousingService } from '../housing-service';
 
 @Component({
   selector: 'app-home',
   imports: [HousingLocation],
-  template: `<section>
+  template: `
+    <section>
       <form>
         <input type="text" placeholder="Filter by city" />
         <button class="primary" type="button">Search</button>
       </form>
     </section>
     <section class="results">
-      <app-housing-location [housingLocation]="housingLocation" />
-    </section>`,
-  styleUrl: './home.css',
+      @for (housingLocation of housingLocationList; track $index) {
+        <app-housing-location [housingLocation]="housingLocation" />
+      }
+    </section>
+  `,
+  styleUrls: ['./home.css'],
 })
 export class Home {
-  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-  housingLocation: HousingLocationInfo = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  housingLocationList: HousingLocationInfo[] = [];
+  housingService: HousingService = inject(HousingService);
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
